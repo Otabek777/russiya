598ceea0c37a0cdbd34627ef7b1063843c017a60
Otabek <otabek.jurayev.2002@gmail.com>
2025-06-05 03:01:11 +0500

    edit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
-$(".header__icon").click(function() {
-    $(".main").toggleClass("close");
-});
-$(".aside__accordeon .btn").click(function() {
+$(".asked__block .btn").click(function(){
     $(this).toggleClass("active");
 });
-$(".color_items .item").click(function() {
-    $(".color_items .item").removeClass("active");
-    $(this).addClass("active");
+function copy() {
+    let copyText = document.querySelector(".codecopy");
+    copyText.select();
+    document.execCommand("copy");
+    $(".codecopybtn").addClass("done");
+    setTimeout(function(){
+        $(".codecopybtn").removeClass("done");
+    }, 2000);
+}
+document.querySelector("#copy").addEventListener("click", copy);
+
+function soundClick() {
+    var audio = document.getElementById('audio');
+    audio.play();
+};
+function soundPauseClick() {
+    var audio = document.getElementById('audio');
+    audio.pause();
+};
+$(".playBtn").click(function() {
+    $(".playBtn").addClass("hide");
+    $(".pauseBtn").removeClass("hide");
+});
+$(".pauseBtn").click(function() {
+    $(".pauseBtn").addClass("hide");
+    $(".playBtn").removeClass("hide");
+});
+
+// modal
+$(".modal-open").click(function() {
+    $(".modal").addClass("active");
+});
+$(".modal__close").click(function() {
+    $(".modal").removeClass("active");
 });
